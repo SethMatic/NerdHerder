50e6451104d80ff684dda2a1458d1ab7cc731e80
Seth Dousman <sdousman3@gmail.com>
2019-12-10 15:25:14 -0500

    Removed Files Not Needed

--- real/models/accounts.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    var account = sequelize.define("Account", {
-      text: DataTypes.STRING,
-      description: DataTypes.TEXT
-    });
-    return account;
-  };
-  
