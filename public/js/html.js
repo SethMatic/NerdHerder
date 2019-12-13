6bf4da9fb73fac3912a8fbc71ac71291175951d6
unknown <seaspatton13@gmail.com>
2019-12-13 17:15:26 -0500

    cookie data stored for username to display on user homepage

--- public/js/html.js
+++ public/js/html.js
@@ -3,29 +3,29 @@ var express = require('express')
 var session = require('express-session')
 var bodyParser = require('body-parser')
 var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'docker',
-    database: 'sheepdb'
-  });
+  host: 'localhost',
+  user: 'root',
+  password: 'docker',
+  database: 'sheepdb'
+});
 var app = express();
 // require('../../routes/userRoutes')(app)
 app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  }));
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-module.exports = function(app) {
-    app.post('/', function (request, response) {
-        var username = request.body.username;
-        connection.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results, fields) {
-            response.contentType('html');
-            response.write(username, $("#displayedUser"));
-            console.log(response, "AsdasdAFASF!!!")
-        })
-        
+module.exports = function (app) {
+  app.post('/', function (request, response) {
+    var username = request.body.username;
+    connection.query('SELECT * FROM accounts WHERE username = ?', [username], function (error, results, fields) {
+      response.contentType('html');
+      // response.write(username, $("#displayedUser"));
+      console.log(response, "AsdasdAFASF!!!")
     })
+
+  })
 }
 
