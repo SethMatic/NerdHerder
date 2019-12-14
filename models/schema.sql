82f4f9f6e9a411e602bd4f08e5ff7e3c5a47b565
Lance <innerlewd@gmail.com>
2019-12-14 11:16:14 -0500

    updated schema

--- models/schema.sql
+++ models/schema.sql
@@ -2,10 +2,14 @@ CREATE DATABASE IF NOT EXISTS `sheepdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 USE `sheepdb`;
 CREATE TABLE IF NOT EXISTS `accounts` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `loggedin` bit NOT NULL DEFAULT FALSE,
+    `loggedin` bit NOT NULL,
     `username` varchar(50) NOT NULL,
     `password` varchar(255) NOT NULL,
     `email` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-INSERT INTO `accounts` (`id`, `loggedin`, `username`, `password`, `email`) VALUES (1, FALSE, 'test', 'test', 'test@testexample23.com');
+CREATE TABLE IF NOT EXISTS `gamedata` (
+	`id` int(11) NOT NULL,
+	`gametitle` varchar(100) NOT NULL,
+    `text` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
